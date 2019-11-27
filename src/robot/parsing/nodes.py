de075de2e5ee989d463c003ac674f19daced7eb2
Janne Härkönen <jth@reaktor.com>
2019-11-27 10:16:15 +0200

    add line numbers to AST nodes

--- src/robot/parsing/nodes.py
+++ src/robot/parsing/nodes.py
@@ -34,19 +34,26 @@ def join_doc_or_meta(lines):
 
 class Node(AST):
     _fields = ()
+    _attributes = ('lineno', 'end_lineno')
+
+    def __init__(self, lineno=0, end_lineno=0):
+        self.lineno = lineno
+        self.end_lineno = end_lineno
 
 
 class MultiValue(Node):
     _fields = ('values',)
 
-    def __init__(self, values):
+    def __init__(self, values, lineno=0, end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         self.values = tuple(values)
 
 
 class SingleValue(Node):
     _fields = ('value',)
 
-    def __init__(self, values):
+    def __init__(self, values, lineno=0, end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         if values and values[0].upper() != 'NONE':
             self.value = values[0]
         else:
@@ -56,28 +63,32 @@ class SingleValue(Node):
 class DataFile(Node):
     _fields = ('sections',)
 
-    def __init__(self, sections):
+    def __init__(self, sections, lineno=0, end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         self.sections = sections
 
 
 class SettingSection(Node):
     _fields = ('settings',)
 
-    def __init__(self, settings):
+    def __init__(self, settings, lineno=0, end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         self.settings = settings
 
 
 class VariableSection(Node):
     _fields = ('variables',)
 
-    def __init__(self, variables):
+    def __init__(self, variables, lineno=0, end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         self.variables = variables
 
 
 class TestCaseSection(Node):
     _fields = ('tests',)
 
-    def __init__(self, tests, header):
+    def __init__(self, tests, header, lineno=0, end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         self.tests = tests
         section_name = normalize_whitespace(header[0]).strip('* ')
         self.tasks = section_name.upper() in ('TASKS', 'TASK')
@@ -86,17 +97,19 @@ class TestCaseSection(Node):
 class KeywordSection(Node):
     _fields = ('keywords',)
 
-    def __init__(self, keywords):
+    def __init__(self, keywords, lineno=0, end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         self.keywords = keywords
 
 
 class Variable(Node):
     _fields = ('name', 'value')
 
-    def __init__(self, name, value):
+    def __init__(self, name, value, lineno=0, end_lineno=0):
         # TODO: Should this be done already by the parser?
         # Applies also to 'WITH NAME', 'NONE' and 'TASK(S)' handling
         # as well as joining doc/meta lines and tuple() conversion.
+        Node.__init__(self, lineno, end_lineno)
         if name.endswith('='):
             name = rstrip(name[:-1])
         self.name = name
@@ -107,7 +120,9 @@ class KeywordCall(Node):
     # TODO: consider `keyword` -> `name`, as in Fixture
     _fields = ('assign', 'keyword', 'args')
 
-    def __init__(self, assign, keyword, args=None):
+    def __init__(self, assign=None, keyword=None, args=None, lineno=0,
+                 end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         self.assign = tuple(assign or ())
         self.keyword = keyword
         self.args = tuple(args or ())
@@ -117,7 +132,9 @@ class ForLoop(Node):
     _fields = ('variables', 'flavor', 'values', 'body')
 
     # TODO: _header and _end are used for deprecation. Remove in RF 3.3!
-    def __init__(self, variables, flavor, values, body=None, _header='FOR'):
+    def __init__(self, variables, flavor, values, body=None, lineno=0,
+                 end_lineno=0, _header='FOR'):
+        Node.__init__(self, lineno, end_lineno)
         self.variables = variables
         self.flavor = normalize_whitespace(flavor)
         self.values = values
@@ -129,7 +146,8 @@ class ForLoop(Node):
 class TestCase(Node):
     _fields = ('name', 'body')
 
-    def __init__(self, name, body):
+    def __init__(self, name, body, lineno=0, end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         self.name = name
         self.body = body
 
@@ -137,7 +155,8 @@ class TestCase(Node):
 class Keyword(Node):
     _fields = ('name', 'body')
 
-    def __init__(self, name, body):
+    def __init__(self, name, body, lineno=0, end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         self.name = name
         self.body = body
 
@@ -145,23 +164,25 @@ class Keyword(Node):
 class TemplateArguments(Node):
     _fields = ('args',)
 
-    def __init__(self, args):
+    def __init__(self, args, lineno=0, end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         self.args = args
 
 
 class ImportSetting(Node):
     _fields = ('name', 'args')
 
-    def __init__(self, name, args):
+    def __init__(self, name, args, lineno=0, end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         self.name = name
         self.args = tuple(args)
 
 
 class LibrarySetting(ImportSetting):
 
-    def __init__(self, name, args):
+    def __init__(self, name, args, lineno=0, end_lineno=0):
         args, alias = self._split_alias(args)
-        ImportSetting.__init__(self, name, args)
+        ImportSetting.__init__(self, name, args, lineno, end_lineno)
         self.alias = alias
 
     def _split_alias(self, args):
@@ -181,21 +202,23 @@ class VariablesSetting(ImportSetting):
 class MetadataSetting(Node):
     _fields = ('name', 'value')
 
-    def __init__(self, name, values):
+    def __init__(self, name, values, lineno=0, end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         self.name = name
         self.value = join_doc_or_meta(values)
 
 
 class DocumentationSetting(SingleValue):
 
-    def __init__(self, values):
-        SingleValue.__init__(self, [join_doc_or_meta(values)])
+    def __init__(self, values, lineno, end_lineno):
+        SingleValue.__init__(self, [join_doc_or_meta(values)], lineno, end_lineno)
 
 
 class Fixture(Node):
     _fields = ('name', 'args')
 
-    def __init__(self, values):
+    def __init__(self, values, lineno=0, end_lineno=0):
+        Node.__init__(self, lineno, end_lineno)
         if values and values[0].upper() != 'NONE':
             self.name = values[0]
             self.args = tuple(values[1:])
--- src/robot/parsing/parser.py
+++ src/robot/parsing/parser.py
@@ -42,12 +42,14 @@ class RobotFrameworkParser(object):
     def p_datafile(self, p):
         '''datafile :
                     | sections'''
-        p[0] = DataFile(p[1] if len(p) == 2 else [])
+        sections = p[1] if len(p) == 2 else []
+        end = sections[-1].end_lineno if sections else 0
+        p[0] = DataFile(sections, lineno=0, end_lineno=end)
 
     def p_sections(self, p):
         '''sections : section
                     | sections section'''
-        append_to_list_value(p)
+        p[0] = combine_values(p)
 
     def p_section(self, p):
         '''section : setting_section
@@ -59,16 +61,20 @@ class RobotFrameworkParser(object):
     def p_setting_section(self, p):
         '''setting_section : setting_header EOS
                            | setting_header EOS settings'''
-        p[0] = SettingSection(p[3] if len(p) == 4 else [])
+        start = p[1]
+        settings = p[3] if len(p) == 4 else []
+        end = settings[-1].end_lineno if settings else start
+        p[0] = SettingSection(settings, start, end)
 
     def p_setting_header(self, p):
         '''setting_header : SETTING_HEADER
                            | setting_header SETTING_HEADER'''
+        p[0] = p.lineno(1)
 
     def p_settings(self, p):
         '''settings : setting
                     | settings setting'''
-        append_to_list_value(p)
+        p[0] = combine_values(p)
 
     def p_setting(self, p):
         '''setting : documentation_setting
@@ -95,154 +101,174 @@ class RobotFrameworkParser(object):
 
     def p_documentation(self, p):
         '''documentation_setting : DOCUMENTATION arguments EOS'''
-        p[0] = DocumentationSetting(p[2])
+        p[0] = DocumentationSetting(*values_and_linespan(p[2]))
 
     def p_suite_setup(self, p):
         '''suite_setup_setting : SUITE_SETUP arguments'''
-        p[0] = SuiteSetupSetting(p[2])
+        p[0] = SuiteSetupSetting(*values_and_linespan(p[2]))
 
     def p_suite_teardown(self, p):
         '''suite_teardown_setting : SUITE_TEARDOWN arguments'''
-        p[0] = SuiteTeardownSetting(p[2])
+        p[0] = SuiteTeardownSetting(*values_and_linespan(p[2]))
 
     def p_metadata(self, p):
         '''metadata_setting : METADATA arguments'''
-        p[0] = MetadataSetting(p[2][0], p[2][1:])
+        name, args, start, end = name_args_and_linespan(p[2])
+        p[0] = MetadataSetting(name, args, start, end)
 
     def p_test_setup(self, p):
         '''test_setup_setting : TEST_SETUP arguments'''
-        p[0] = TestSetupSetting(p[2])
+        p[0] = TestSetupSetting(*values_and_linespan(p[2]))
 
     def p_test_teardown(self, p):
         '''test_teardown_setting : TEST_TEARDOWN arguments'''
-        p[0] = TestTeardownSetting(p[2])
+        p[0] = TestTeardownSetting(*values_and_linespan(p[2]))
 
     def p_test_template(self, p):
         '''test_template_setting : TEST_TEMPLATE arguments'''
-        p[0] = TestTemplateSetting(p[2])
+        p[0] = TestTemplateSetting(*values_and_linespan(p[2]))
 
     def p_test_timeout(self, p):
         '''test_timeout_setting : TEST_TIMEOUT arguments'''
-        p[0] = TestTimeoutSetting(p[2])
+        p[0] = TestTimeoutSetting(*values_and_linespan(p[2]))
 
     def p_force_tags(self, p):
         '''force_tags_setting : FORCE_TAGS arguments'''
-        p[0] = ForceTagsSetting(p[2])
+        p[0] = ForceTagsSetting(*values_and_linespan(p[2]))
 
     def p_default_tags(self, p):
         '''default_tags_setting : DEFAULT_TAGS arguments'''
-        p[0] = DefaultTagsSetting(p[2])
+        p[0] = DefaultTagsSetting(*values_and_linespan(p[2]))
 
     def p_library(self, p):
         '''library_setting : LIBRARY arguments'''
-        name, args = parse_name_and_args(p)
-        p[0] = LibrarySetting(name, args)
+        name, args, start, end = name_args_and_linespan(p[2])
+        p[0] = LibrarySetting(name, args, start, end)
 
     def p_resource(self, p):
         '''resource_setting : RESOURCE arguments'''
-        name, args = parse_name_and_args(p)
-        p[0] = ResourceSetting(name, args)
+        name, args, start, end = name_args_and_linespan(p[2])
+        p[0] = ResourceSetting(name, args, start, end)
 
     def p_variables_import(self, p):
         '''variables_setting : VARIABLES arguments'''
-        name, args = parse_name_and_args(p)
-        p[0] = VariablesSetting(name, args)
+        name, args, start, end = name_args_and_linespan(p[2])
+        p[0] = VariablesSetting(name, args, start, end)
 
     def p_setup(self, p):
         '''setup_setting : SETUP arguments'''
-        p[0] = SetupSetting(p[2])
+        p[0] = SetupSetting(*values_and_linespan(p[2]))
 
     def p_teardown(self, p):
         '''teardown_setting : TEARDOWN arguments'''
-        p[0] = TeardownSetting(p[2])
+        p[0] = TeardownSetting(*values_and_linespan(p[2]))
 
     def p_template(self, p):
         '''template_setting : TEMPLATE arguments'''
-        p[0] = TemplateSetting(p[2])
+        p[0] = TemplateSetting(*values_and_linespan(p[2]))
 
     def p_timeout(self, p):
         '''timeout_setting : TIMEOUT arguments'''
-        p[0] = TimeoutSetting(p[2])
+        p[0] = TimeoutSetting(*values_and_linespan(p[2]))
 
     def p_tags(self, p):
         '''tags_setting : TAGS arguments'''
-        p[0] = TagsSetting(p[2])
+        p[0] = TagsSetting(*values_and_linespan(p[2]))
 
     def p_arguments_setting(self, p):
         '''arguments_setting : ARGUMENTS arguments'''
-        p[0] = ArgumentsSetting(p[2])
+        p[0] = ArgumentsSetting(*values_and_linespan(p[2]))
 
     def p_return(self, p):
         '''return_setting : RETURN arguments'''
-        p[0] = ReturnSetting(p[2])
+        p[0] = ReturnSetting(*values_and_linespan(p[2]))
 
     def p_variable_section(self, p):
         '''variable_section : variable_header EOS
                             | variable_header EOS variables'''
-        p[0] = VariableSection(p[3] if len(p) == 4 else [])
+        start = p[1]
+        variables = p[3] if len(p) == 4 else []
+        end = variables[-1].end_lineno if variables else start
+        p[0] = VariableSection(variables, start, end)
 
     def p_variable_header(self, p):
         '''variable_header : VARIABLE_HEADER
                            | variable_header VARIABLE_HEADER'''
+        p[0] = p.lineno(1)
 
     def p_variables(self, p):
         '''variables : variable
                      | variables variable'''
-        append_to_list_value(p)
+        p[0] = combine_values(p)
 
     def p_variable(self, p):
         '''variable : VARIABLE arguments EOS'''
-        p[0] = Variable(p[1], p[2])
+        p[0] = Variable(p[1], *values_and_linespan(p[2]))
 
     def p_testcase_section(self, p):
         '''testcase_section : testcase_header EOS
                             | testcase_header EOS tests'''
-        p[0] = TestCaseSection(p[3] if len(p) == 4 else [], p[1])
+        header_values, start, _ = values_and_linespan(p[1])
+        tests = p[3] if len(p) == 4 else []
+        end = tests[-1].end_lineno if tests else start
+        p[0] = TestCaseSection(tests, header_values, start, end)
 
     def p_testcase_header(self, p):
         '''testcase_header : TESTCASE_HEADER
                            | testcase_header TESTCASE_HEADER'''
-        append_to_list_value(p)
+        p[0] = combine_values_with_lineno(p)
 
     def p_keyword_section(self, p):
         '''keyword_section : keyword_header EOS
                            | keyword_header EOS keywords'''
-        p[0] = KeywordSection(p[3] if len(p) == 4 else [])
+        start = p[1]
+        keywords = p[3] if len(p) == 4 else []
+        end = keywords[-1].end_lineno if keywords else start
+        p[0] = KeywordSection(keywords, start, end)
 
     def p_keyword_header(self, p):
         '''keyword_header : KEYWORD_HEADER
                           | keyword_header KEYWORD_HEADER'''
+        p[0] = p.lineno(1)
 
     def p_tests(self, p):
         '''tests : test
                  | tests test'''
-        append_to_list_value(p)
+        p[0] = combine_values(p)
 
     def p_keywords(self, p):
         '''keywords : keyword
                     | keywords keyword'''
-        append_to_list_value(p)
+        p[0] = combine_values(p)
 
     def p_test(self, p):
         '''test : NAME EOS
                 | NAME EOS body_items'''
+        start = p.lineno(1)
         if len(p) == 3:
-            p[0] = TestCase(p[1], [])
+            body = []
+            end = start
         else:
-            p[0] = TestCase(p[1], p[3])
+            body = p[3]
+            end = body[-1].end_lineno
+        p[0] = TestCase(p[1], body, lineno=start, end_lineno=end)
 
     def p_keyword(self, p):
         '''keyword : NAME EOS
                    | NAME EOS body_items'''
+        start = p.lineno(1)
         if len(p) == 3:
-            p[0] = Keyword(p[1], [])
+            body = []
+            end = start
         else:
-            p[0] = Keyword(p[1], p[3])
+            body = p[3]
+            end = body[-1].end_lineno
+        p[0] = Keyword(p[1], body, lineno=start, end_lineno=end)
 
     def p_body_items(self, p):
         '''body_items : body_item
                       | body_items body_item'''
-        append_to_list_value(p)
+        p[0] = combine_values(p)
 
     def p_body_item(self, p):
         '''body_item : forloop
@@ -252,17 +278,21 @@ class RobotFrameworkParser(object):
                      | invalid_forloop'''
         p[0] = p[1]
 
+    # TODO: rename -> keyword_call
     def p_step(self, p):
         '''step : KEYWORD arguments EOS'''
-        p[0] = KeywordCall(None, p[1], p[2])
+        arguments, _, end_lineno = values_and_linespan(p[2])
+        p[0] = KeywordCall(None, p[1], arguments, p.lineno(1), end_lineno)
 
     def p_step_with_assignment(self, p):
         '''step : assignments EOS
                 | assignments KEYWORD arguments EOS'''
+        assignments, lineno, end_lineno = values_and_linespan(p[1])
         if len(p) == 3:
-            p[0] = KeywordCall(p[1], None)
+            p[0] = KeywordCall(assignments, lineno=lineno, end_lineno=end_lineno)
         else:
-            p[0] = KeywordCall(p[1], p[2], p[3])
+            arguments, _, end_lineno = values_and_linespan(p[3])
+            p[0] = KeywordCall(assignments, p[2], arguments, lineno=lineno, end_lineno=end_lineno)
 
     def p_forloop(self, p):
         '''forloop : for_header for_body END EOS
@@ -271,32 +301,37 @@ class RobotFrameworkParser(object):
         if len(p) == 3: # FIXME: Better way to handle dangling END
             p[0] = KeywordCall(None, p[1])
         elif len(p) == 4:
-            p[1]._end = p[2]    # TODO: Used for deprecation. Remove in RF 3.3!
             p[0] = p[1]
+            p[0].end_lineno = p.lineno(3)
+            p[0]._end = p[2]    # TODO: Used for deprecation. Remove in RF 3.3!
         else:
-            p[1].body = p[2]
-            p[1]._end = p[3]    # TODO: Used for deprecation. Remove in RF 3.3!
             p[0] = p[1]
+            p[0].body = p[2]
+            p[0].end_lineno = p.lineno(4)
+            p[0]._end = p[3]    # TODO: Used for deprecation. Remove in RF 3.3!
 
     def p_for_header(self, p):
         '''for_header : FOR arguments FOR_SEPARATOR arguments EOS
                       | FOR arguments EOS'''
         # TODO: _header is used for deprecating ':FOR' style. Remove in RF 3.3!
+        start = p.lineno(1)
+        variables = values_and_linespan(p[2])[0]
         if len(p) == 6:
-            p[0] = ForLoop(p[2], p[3], p[4], _header=p[1])
+            values = values_and_linespan(p[4])[0]
+            p[0] = ForLoop(variables, p[3], values, lineno=start, _header=p[1])
         else:
-            p[0] = ForLoop(p[2], 'IN', [], _header=p[1])
+            p[0] = ForLoop(variables, 'IN', [], lineno=start, _header=p[1])
 
     def p_for_body(self, p):
         '''for_body : step
                     | for_body step
                     | templatearguments
                     | for_body templatearguments'''
-        append_to_list_value(p)
+        p[0] = combine_values(p)
 
     def p_templatearguments(self, p):
         '''templatearguments : arguments EOS'''
-        p[0] = TemplateArguments(p[1])
+        p[0] = TemplateArguments(*values_and_linespan(p[1]))
 
     def p_invalid_for_loop(self, p):
         '''invalid_forloop : for_header for_body'''
@@ -305,7 +340,7 @@ class RobotFrameworkParser(object):
     def p_assignments(self, p):
         '''assignments : ASSIGN
                        | assignments ASSIGN'''
-        append_to_list_value(p)
+        p[0] = combine_values_with_lineno(p)
 
     def p_arguments(self, p):
         '''arguments : args
@@ -315,7 +350,7 @@ class RobotFrameworkParser(object):
     def p_args(self, p):
         '''args : ARGUMENT
                 | arguments ARGUMENT'''
-        append_to_list_value(p)
+        p[0] = combine_values_with_lineno(p)
 
     def p_empty_arg(self, p):
         '''empty_arg : '''
@@ -329,21 +364,37 @@ class RobotFrameworkParser(object):
         #self.parser.errok()
 
 
-def append_to_list_value(p):
+def combine_values(p):
     if len(p) == 1:
-        p[0] = []
-    elif len(p) == 2:
-        p[0] = [p[1]]
-    else:
-        value = p[1]
-        # TODO: IS this check needed??
-        if p[2] is not None:
-            value.append(p[2])
-        p[0] = value
+        return []
+    if len(p) == 2:
+        return [p[1]]
+    value = p[1]
+    value.append(p[2])
+    return value
 
 
-def parse_name_and_args(p):
-    if p[2]:
-        return p[2][0], p[2][1:]
-    else:
-        return '', []
+def combine_values_with_lineno(p):
+    if len(p) == 1:
+        return []
+    if len(p) == 2:
+        return [(p[1], p.lineno(1))]
+    value = p[1]
+    value.append((p[2], p.lineno(2)))
+    return value
+
+
+def values_and_linespan(values_with_linenos):
+    if values_with_linenos:
+        values = [v[0] for v in values_with_linenos]
+        start = values_with_linenos[0][1]
+        end = values_with_linenos[-1][1]
+        return values, start, end
+    return [], 0, 0
+
+
+def name_args_and_linespan(values_with_linenos):
+    if values_with_linenos:
+        value, start, end = values_and_linespan(values_with_linenos)
+        return value[0], value[1:], start, end
+    return '', [], 0, 0
